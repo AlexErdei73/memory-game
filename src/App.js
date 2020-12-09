7cbee2772d057bf8440e6a64f637fe014e7e47e5
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-12-09 11:37:23 +0000

    add basic template to Add component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const initialState = {
+    physicists: [
+      {
+        name: "Isaac Newton",
+        from: "1643",
+        to: "1727",
+        image: "../Public/images/isaac-newton.jpg",
+      },
+    ],
+  };
+
+  const [state, setState] = useState(initialState);
+
+  return (
+    <div className="App">
+      <div className="cardContainer">
+        <div className="row">first line</div>
+        <div className="row">second line</div>
+        <div className="row">third line</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
