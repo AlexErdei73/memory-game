75bcdec14a2077074ecb643d3f8e4b577ec4251e
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-12-16 09:45:06 +0000

    make title bar smaller for mobile

--- src/components/titleBar.jsx
+++ src/components/titleBar.jsx
@@ -1,12 +1,12 @@
-import { Navbar, Container, Col } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
 
 const TitleBar = (props) => {
   return (
-    <Navbar className="bg-info text-light" fixed="top">
+    <Navbar className="bg-info text-light font-size-xlg" fixed="top">
       <Container>
-        <h1>Great Physicists</h1>
-        <h2>Score: {props.score}</h2>
-        <h2>Top Score: {props.highestScore}</h2>
+        <h4>Great Physicists</h4>
+        <h4>Score: {props.score}</h4>
+        <h4>Top Score: {props.highestScore}</h4>
       </Container>
     </Navbar>
   );
